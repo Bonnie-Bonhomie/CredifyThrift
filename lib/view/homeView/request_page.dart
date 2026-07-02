20ea6dbebf94704033e76f08992236353fed0b1e
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-07-02 21:21:23 +0100

    Declared request page

---
+++ lib/view/homeView/request_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RequestPageView extends StatelessWidget {
+  const RequestPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Request page')),
+    );
+  }
+}
