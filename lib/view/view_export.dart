b1b501bfa5c57fb257fdd27ff4cbe8ac9e336679
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-07-02 21:49:14 +0100

    View barrel export

---
+++ lib/view/view_export.dart
@@ -0,0 +1,19 @@
+export 'homeView/history_page.dart';
+export 'homeView/home_page.dart';
+export 'homeView/main_screen.dart';
+export 'homeView/profile_page.dart';
+export 'homeView/scan_page.dart';
+export 'homeView/request_page.dart';
+
+
+export 'onboardViews/auth_views/create_password.dart';
+export 'onboardViews/auth_views/sign_up_view.dart';
+export 'onboardViews/auth_views/verify_acc.dart';
+export 'onboardViews/identifyVerification/camera_page.dart';
+export 'onboardViews/identifyVerification/verify_method.dart';
+export 'onboardViews/personal_details/address_details.dart';
+export 'onboardViews/personal_details/personal_info_view.dart';
+
+
+export 'view_widgets/gradient_page.dart';
+export 'view_widgets/shared_widget.dart';
