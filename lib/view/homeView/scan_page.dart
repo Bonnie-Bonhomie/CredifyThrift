0fa61e82411ad8dc0024b4be8356602cc416c697
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-07-02 21:19:31 +0100

    Added scan page

---
+++ lib/view/homeView/scan_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScanPageView extends StatelessWidget {
+  const ScanPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Scan page')),
+    );
+  }
+}
