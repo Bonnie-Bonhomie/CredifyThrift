9e0118d6634c23373289ab72f1e071ad88b1c0db
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-07-02 21:48:13 +0100

    View model barrel export

---
+++ lib/viewModel/view_model_export.dart
@@ -0,0 +1,5 @@
+export 'package:credify/viewModel/auth_view_model/create_pwd_v_model.dart';
+export 'package:credify/viewModel/auth_view_model/details_view_model.dart';
+export 'package:credify/viewModel/auth_view_model/sign_in_v_model.dart';
+
+export 'loader_model.dart';
