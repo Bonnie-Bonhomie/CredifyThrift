006513f1284cb7d9b3cd8fec64e0c331982abee1
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-07-02 11:32:10 +0100

    Added new color and modified existing

--- lib/core/constants/app_color.dart
+++ lib/core/constants/app_color.dart
@@ -6,12 +6,15 @@ class AppColors {
   static final Color gradientBtn = Color(0xFF100367);
   static const Color darkBlue = Color(0xFF0C0269);
 
-  static const Color lightBackground = Color(0xFFDDDDF8);
+  static const Color lightBackground = Color(0xFFEBEEFD);
   static const Color darkBackground = Color(0xFF000010);
 
   static const Color lightGrey = Color(0xFFF8F8FA);
-  static const Color darkGrey = Color(0xF21F2E59);
+  static const Color darkGrey = Color(0xF212182A);
 
   static const Color blueGrey = Color(0xFF00FFFF);
-  static const Color disableColor = Color(0xF0212223);
+  static const Color disableColor = Color(0xF03F5265);
+  static const Color onSurface = Color(0xFFF3F3FD);
+  static const Color surface = Color(0xFF01012A);
+  
 }
