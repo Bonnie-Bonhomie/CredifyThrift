a0d552e2e9b0eac6754f4d076d8f676758af4bb1
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-07-02 18:54:40 +0100

    Change the text style and color

--- lib/core/widgets/term_or_agree.dart
+++ lib/core/widgets/term_or_agree.dart
@@ -13,16 +13,17 @@ class TermOfAgreeText extends StatelessWidget {
       textAlign: TextAlign.center,
       text: TextSpan(
         text: 'By using our mobile app, you agree to our ',
+        style: Theme.of(context).textTheme.bodyMedium,
         children: [
           TextSpan(
               text: 'Term of use',
-              style: TextStyle(decoration: TextDecoration.underline),
+              style: TextStyle(decoration: TextDecoration.underline, fontWeight: FontWeight.bold),
               recognizer: TapGestureRecognizer()..onTap = (){}
           ),
           TextSpan(text: ' and '),
           TextSpan(
               text: 'Privacy policy.',
-              style: TextStyle(decoration: TextDecoration.underline),
+              style: TextStyle(decoration: TextDecoration.underline, fontWeight: FontWeight.bold),
               recognizer: TapGestureRecognizer()..onTap = (){}
           ),
         ],
