0b27a4b47ae94dc688edcc853f1967cb6e606316
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-07-02 21:30:02 +0100

    Declared history page

---
+++ lib/view/homeView/history_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HistoryPageView extends StatelessWidget {
+  const HistoryPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('History Page')),
+    );
+  }
+}
