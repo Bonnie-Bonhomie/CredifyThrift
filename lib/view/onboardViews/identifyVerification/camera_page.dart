2ba14774b271b65306f0f922aa06f7b4ee5bf207
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-07-02 11:33:54 +0100

    Added permission checker

--- lib/view/onboardViews/identifyVerification/camera_page.dart
+++ lib/view/onboardViews/identifyVerification/camera_page.dart
@@ -1,6 +1,10 @@
+import 'package:camera/camera.dart';
+import 'package:credify/config/AppRoutes/routes.dart';
+import 'package:credify/core/constants/app_color.dart';
+import 'package:credify/core/widgets/app_button.dart';
 import 'package:credify/main.dart';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -15,14 +19,37 @@ class _CameraPageState extends State<CameraPage> {
   @override
   void initState() {
     super.initState();
-    initCamera();
+    _checkCameraPermission();
   }
 
   Future<void> initCamera() async {
-    controller = CameraController(cameraRoles[0], ResolutionPreset.high);
+    print('Hello');
+    controller = CameraController(cameraRoles.first, ResolutionPreset.high);
     await controller!.initialize();
     if (!mounted) return;
     setState(() {});
+  }
+
+  Future<void> _checkCameraPermission() async {
+    final status = await Permission.camera.request();
+
+    if (status.isGranted) {
+      // Initialize camera here
+      initCamera();
+    } else if (status.isDenied) {
+      // User denied permission
+      if (mounted) {
+        Navigator.pop(context);
+      }
+    } else if (status.isPermanentlyDenied) {
+      if (mounted) {
+        Navigator.pop(context);
+
+        Future.delayed(const Duration(milliseconds: 300), () {
+          openAppSettings();
+        });
+      }
+    }
   }
 
   @override
@@ -33,6 +60,10 @@ class _CameraPageState extends State<CameraPage> {
 
   @override
   Widget build(BuildContext context) {
+    print('Hi');
+    // if(!controller!.value.isInitialized){
+    //   Navigator.pop(context);
+    // }
     if (controller == null || !controller!.value.isInitialized) {
       return const Scaffold(
         backgroundColor: Colors.black,
@@ -42,6 +73,7 @@ class _CameraPageState extends State<CameraPage> {
 
     return Scaffold(
       backgroundColor: Colors.black,
+      appBar: AppBar(leading: IconButton(onPressed: (){Navigator.pop(context);}, icon: Icon(Icons.arrow_back_ios))),
       body: Stack(
         children: [
           CameraPreview(controller!),
@@ -50,7 +82,7 @@ class _CameraPageState extends State<CameraPage> {
           Center(
             child: Container(
               width: 300,
-              height: 180,
+              height: 250,
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.cyan, width: 2),
                 borderRadius: BorderRadius.circular(10),
@@ -91,7 +123,7 @@ class _CameraPageState extends State<CameraPage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Icon(Icons.flash_off, color: Colors.white),
+                Icon(Icons.flash_off, color: Colors.grey),
                 GestureDetector(
                   onTap: () async {
                     await controller!.takePicture();
@@ -102,10 +134,11 @@ class _CameraPageState extends State<CameraPage> {
                     decoration: BoxDecoration(
                       border: Border.all(color: Colors.white, width: 4),
                       shape: BoxShape.circle,
+                      color: AppColors.primary
                     ),
                   ),
                 ),
-                Icon(Icons.camera_alt, color: Colors.white),
+                Icon(Icons.done, color: Colors.white),
               ],
             ),
           ),
@@ -114,3 +147,31 @@ class _CameraPageState extends State<CameraPage> {
     );
   }
 }
+
+
+class CheckQuality extends StatelessWidget {
+  const CheckQuality({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Column(
+        children: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios)),
+          const SizedBox(),
+          
+          Container(),
+          
+          Text('Chaek Quality'),
+          Text('Please make sure your card is clear to read with no blur or glare', textAlign: TextAlign.center,),
+
+          Icon(Icons.lens),
+          AppButton(onPressed: (){}, label: 'All clear, continue'),
+
+          TextButton(onPressed: (){Navigator.pushNamed(context, Routes.camera);}, child: Text('Take a new photo')),
+        ],
+      )),
+    );
+  }
+}
+
