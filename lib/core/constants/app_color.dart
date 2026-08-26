027989fc764c42aeb89c7c7ee2178808e6f29307
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-26 01:21:20 +0100

    Added new colors

--- lib/core/constants/app_color.dart
+++ lib/core/constants/app_color.dart
@@ -5,9 +5,15 @@ class AppColors {
   static const  Color primary = Color(0xFF0730FD);
   static final Color gradientBtn = Color(0xFF100367);
   static const Color darkBlue = Color(0xFF0C0269);
+  static const Color lightBlue = Color(0xFF5D4DF6);
+
+  static const Color progressColor = Color(0xFF0394D3);
 
   static const Color lightBackground = Color(0xFFF6F3EC);
   static const Color darkBackground = Color(0xFF000010);
+
+  static const Color textMain = Color(0xFF02021C);
+  static const Color textSecondary = Color(0xFF9090D7);
 
   static const Color lightGrey = Color(0xFFF8F8FA);
   static const Color darkGrey = Color(0xFF12182A);
