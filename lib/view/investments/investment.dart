518442fe00196668451172fcd01ec9d9120c2b76
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-26 01:14:39 +0100

    Changed the history page to investment page

--- lib/view/investments/investment.dart
+++ lib/view/investments/investment.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class HistoryPageView extends StatelessWidget {
-  const HistoryPageView({super.key});
+class InvestPageView extends StatelessWidget {
+  const InvestPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('History Page')),
+      body: Center(child: Text('Investment Page')),
     );
   }
 }
