d309341b02ece73a66bee7fd49bbaa3099a38875
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-26 01:21:56 +0100

    Created a  custom text style for the app

---
+++ lib/core/utils/Helpers/cred_textstyle.dart
@@ -0,0 +1,66 @@
+import 'package:credify/export_barrel.dart';
+
+class CredTextStyle {
+  CredTextStyle._();
+  
+  static final TextStyle h1 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    //color: App//colors.textMain
+  );
+  
+  static final TextStyle h2 = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+    //color: App//colors.textMain
+  );
+  
+  static final TextStyle h3 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    //color: App//colors.textMain
+  );
+  
+  static final TextStyle h4 = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.bold,
+    //color: App//colors.textMain
+  );
+  
+  static final TextStyle h5 = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+    //color: App//colors.textMain
+  );
+  
+  static final TextStyle h6 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+    //color: App//colors.textMain
+  );
+  
+  static final TextStyle bs = TextStyle(
+    fontSize: 22,
+    //color: App//colors.textMain
+  );
+  
+  static final TextStyle bs1 = TextStyle(
+    fontSize: 20,
+    //color: App//colors.textMain
+  );
+  
+  static final TextStyle bs2 = TextStyle(
+    fontSize: 17,
+  ); 
+  
+  static final TextStyle bs3 = TextStyle(
+    fontSize: 15,
+  ); 
+  
+  static final TextStyle bs4 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w200
+  );
+  
+  
+}
