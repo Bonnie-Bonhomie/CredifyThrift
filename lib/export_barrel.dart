4f1264fed88e257d22b1b59070a9c3c41cfaeaad
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-26 01:18:55 +0100

    Create a barrel for all export

---
+++ lib/export_barrel.dart
@@ -0,0 +1,63 @@
+export 'config/AppRoutes/app_routes.dart';
+export 'config/AppRoutes/page_slider.dart';
+export 'config/AppRoutes/routes.dart';
+
+
+export 'core/constants/app_size.dart';
+export 'core/constants/app_keys.dart';
+export 'core/constants/app_color.dart';
+
+export 'core/Theme/app_spacing.dart';
+export 'core/Theme/app_theme.dart';
+export 'core/Theme/color_theme.dart';
+export 'core/Theme/dark_theme.dart';
+export 'core/Theme/form_field_theme.dart';
+export 'core/Theme/light_theme.dart';
+export 'core/Theme/text_theme.dart';
+
+export 'core/utils/Helpers/cred_textstyle.dart';
+export 'core/utils/Helpers/dismiss_key_board.dart';
+
+export 'core/utils/loaderFile/loading_spinkit.dart';
+export 'core/utils/loaderFile/loading_wrapper.dart';
+export 'core/utils/validator/validator.dart';
+export 'core/utils/custom_app_bar.dart';
+export 'core/utils/custom_snack_bar.dart';
+export 'core/utils/date_formatter.dart';
+export 'core/utils/form_widget.dart';
+export 'core/utils/permission_page.dart';
+export 'core/utils/persmission_box.dart';
+export 'core/utils/phone_number_formatter.dart';
+
+export 'data/models/countries_model.dart';
+// export 'data/repositories/';
+
+export 'dependencies/auth_dependency.dart';
+export 'dependencies/core_dependencies.dart';
+export 'dependencies/dependencie_injection.dart';
+
+export 'view/investments/investment.dart';
+export 'view/homeView/home_page.dart';
+export 'view/homeView/main_screen.dart';
+export 'view/homeView/profile_page.dart';
+export 'view/activities/activity.dart';
+export 'view/savings/saving_page.dart';
+
+export 'view/onboardViews/auth_views/create_password.dart';
+export 'view/onboardViews/auth_views/sign_up_view.dart';
+export 'view/onboardViews/auth_views/verify_acc.dart';
+
+export 'view/onboardViews/identifyVerification/camera_page.dart';
+export 'view/onboardViews/identifyVerification/verify_method.dart';
+
+export 'view/onboardViews/personal_details/address_details.dart';
+export 'view/onboardViews/personal_details/personal_info_view.dart';
+
+export 'view/view_export.dart';
+export 'viewModel/view_model_export.dart';
+
+export 'package:credify/core/widgets/app_button.dart';
+export 'package:flutter/material.dart';
+export 'package:flutter/services.dart';
+export 'package:provider/provider.dart';
+
