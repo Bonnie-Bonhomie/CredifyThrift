46965e40490b3a9cc609737dd6e4aeeb6c742fff
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-26 01:19:38 +0100

    Linear progress bar with gradient

---
+++ lib/core/utils/gradient_linear_bar.dart
@@ -0,0 +1,48 @@
+
+
+import '../../export_barrel.dart';
+
+class GradientProgressBar extends StatelessWidget {
+  final double progress; // 0.0 - 1.0
+
+  const GradientProgressBar({
+    super.key,
+    required this.progress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
+          height: 10,
+          decoration: BoxDecoration(
+            color: AppColors.lightBlue.withAlpha(100),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Stack(
+            children: [
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                width: constraints.maxWidth * progress,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: AppColors.progressColor
+                  // gradient: const LinearGradient(
+                  //   colors: [
+                  //     Colors.green,
+                  //     Colors.blue,
+                  //   ],
+                  //   begin: Alignment.topCenter,
+                  //   end: Alignment.bottomCenter
+                  // ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
