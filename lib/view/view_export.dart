54e8b89697a492541e41153d492a978484511b3e
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-27 12:09:35 +0100

    Modified

--- lib/view/view_export.dart
+++ lib/view/view_export.dart
@@ -2,7 +2,7 @@ export 'investments/investment.dart';
 export 'homeView/home_page.dart';
 export 'homeView/main_screen.dart';
 export 'homeView/profile_page.dart';
-export 'savings/saving_page.dart';
+export 'savings/presentation/saving_page.dart';
 export 'activities/activity.dart';
 
 
