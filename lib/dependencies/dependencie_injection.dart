193548c9647f44b586a551861bc494b4d364da63
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-28 11:10:41 +0100

    Added saving notifier

--- lib/dependencies/dependencie_injection.dart
+++ lib/dependencies/dependencie_injection.dart
@@ -1,5 +1,7 @@
 import 'package:credify/dependencies/auth_dependency.dart';
 import 'package:credify/dependencies/core_dependencies.dart';
+import 'package:credify/export_barrel.dart';
+import 'package:credify/view/savings/notifier/saving_notifier.dart';
 import 'package:provider/single_child_widget.dart';
 
 class AppDependencies{
@@ -7,10 +9,11 @@ class AppDependencies{
   static final List<SingleChildWidget> providers = [
     ...CoreDependencies.providers,
     ...AuthDependencies.providers,
+    ...provider,
   ];
 }
 
 
   final List<SingleChildWidget> provider =[
-
+      ChangeNotifierProvider<SavingNotifier>(create: (context) => SavingNotifier()),
 ];
