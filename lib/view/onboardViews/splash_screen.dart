9e1d05f635f76191a4d9474e26b095cc298868e0
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-28 23:44:17 +0100

    Create splash screen

---
+++ lib/view/onboardViews/splash_screen.dart
@@ -0,0 +1,17 @@
+import '../../export_barrel.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Text('Credify.')),
+    );
+  }
+}
