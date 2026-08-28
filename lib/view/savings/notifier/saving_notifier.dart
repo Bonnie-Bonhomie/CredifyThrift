ee757a7f091c91f3a2889327568b7319fd9b1516
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-29 00:05:47 +0100

    Added mock data for saving method

--- lib/view/savings/notifier/saving_notifier.dart
+++ lib/view/savings/notifier/saving_notifier.dart
@@ -14,7 +14,8 @@ class SavingNotifier extends ChangeNotifier {
       updatedAt: DateTime.now().toString(),
       duration: '16',
       paidAmount: 500,
-    ),SaveModel(
+    ),
+    SaveModel(
       title: 'Buy a house in new york',
       subtitle: 'To buy a new house',
       frequency: Frequency.monthly,
@@ -23,7 +24,8 @@ class SavingNotifier extends ChangeNotifier {
       updatedAt: DateTime.now().toString(),
       duration: '2',
       paidAmount: 1500,
-    ),SaveModel(
+    ),
+    SaveModel(
       title: 'Buy a house in new york',
       subtitle: 'To buy a new house',
       frequency: Frequency.weekly,
@@ -32,7 +34,8 @@ class SavingNotifier extends ChangeNotifier {
       updatedAt: DateTime.now().toString(),
       duration: '7',
       paidAmount: 300,
-    ),SaveModel(
+    ),
+    SaveModel(
       title: 'Buy a house in new york',
       subtitle: 'To buy a new house',
       duration: '9',
@@ -62,5 +65,37 @@ class SavingNotifier extends ChangeNotifier {
       duration: '5',
       paidAmount: 2000,
     ),
+  ];
+
+  List<SaveModeModel> saveMode = [
+    // SaveModeModel(
+    //   title: 'Other Expenses',
+    //   frequency: Frequency.monthly,
+    //   amount: 130000,
+    //   duration: 12,
+    // ),
+    SaveModeModel(
+      title: 'Food and Goceries',
+      frequency: Frequency.monthly,
+      amount: 10000,
+      duration: 4,
+    ),
+    SaveModeModel(
+      title: 'School Fees',
+      frequency: Frequency.weekly,
+      amount: 230000,
+      duration: 30,
+    ),
+    SaveModeModel(
+      title: 'Traveling',
+      frequency: Frequency.monthly,
+      amount: 10000,
+      duration: 4,
+    ),
+
+    // SaveModeModel(title: 'Other Expenses', frequency: Frequency.monthly, amount: 125000, duration: 4, average: 150),
+    // SaveModeModel(title: 'Other Expenses', frequency: Frequency.monthly, amount: 34000, duration: 4, average: 150),
+    // SaveModeModel(title: 'Other Expenses', frequency: Frequency.monthly, amount: 30000, duration: 4, average: 150),
+    // SaveModeModel(title: 'Other Expenses', frequency: Frequency.monthly, amount: 20000, duration: 4, average: 150),
   ];
 }
