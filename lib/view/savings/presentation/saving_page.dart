57b12405eee93e467456d7e7e40be64e3a25e306
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-08-28 11:04:12 +0100

    Modified

--- lib/view/savings/presentation/saving_page.dart
+++ lib/view/savings/presentation/saving_page.dart
@@ -1,3 +1,4 @@
+import 'package:credify/view/savings/notifier/saving_notifier.dart';
 import 'package:credify/viewModel/app_model.dart';
 
 import '../../../core/utils/gradient_linear_bar.dart';
@@ -15,6 +16,7 @@ class _SavingPageState extends State<SavingPage> {
 
   @override
   Widget build(BuildContext context) {
+    final provider = context.watch<SavingNotifier>();
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -23,17 +25,8 @@ class _SavingPageState extends State<SavingPage> {
               clipBehavior: Clip.none,
               alignment: Alignment.center,
               children: [
-                Container(
-                  height: 330,
-                  margin: const EdgeInsets.only(bottom: 30),
-                  padding: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [AppColors.primary, AppColors.gradientBtn],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ),
-                  ),
+                GradientContainer(
+                  height: 320,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -59,7 +52,7 @@ class _SavingPageState extends State<SavingPage> {
                         ],
                       ),
 
-                      const SizedBox(height: 30,),
+                      const SizedBox(height: 30),
                       const Text(
                         'Total Amount Saved',
                         style: TextStyle(
@@ -69,104 +62,71 @@ class _SavingPageState extends State<SavingPage> {
                         ),
                       ),
                       const SizedBox(height: 10),
-                      Text(appModel.formatCurrency(12756), style: CredTextStyle.h2.copyWith(color: AppColors.onSurface),),
+                      Text(
+                        appModel.formatCurrency(12756),
+                        style: CredTextStyle.h2.copyWith(
+                          color: AppColors.onSurface,
+                        ),
+                      ),
                       const SizedBox(height: 10),
-                      Text('Last edited 2 days ago',
-                          style: CredTextStyle.bs3.copyWith(
-                              color: AppColors.textSecondary)),
+                      Text(
+                        'Last edited 2 days ago',
+                        style: CredTextStyle.bs3.copyWith(
+                          color: AppColors.textSecondary,
+                        ),
+                      ),
 
-                      const SizedBox(height: 10,),
-                      SizedBox(
-                          width: 200,
-                          child: GradientButton()),
+                      const SizedBox(height: 10),
+                      SizedBox(width: 200,
+                          child: GradientButton(
+                            title: 'create a new goal', iconSize: 20,)),
+                      // const SizedBox(height: 10,)
                     ],
                   ),
                 ),
                 Positioned(
-                  top: 250,
+                  top: 240,
                   child: Container(
-                    width: MediaQuery
-                        .of(context)
-                        .size
-                        .width,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 9,
-                    ),
-                    // margin: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: List.generate(8, (index) {
-                        return Card(
-                            child: SizedBox(
-                              height: 170,
-                              child: Padding(
-                                padding: const EdgeInsets.all(18.0),
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 9,
+                      ),
+                      // margin: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Consumer<SavingNotifier>(
+                          builder: (key, saving, child) {
+                            List save = saving.savings;
+                            if(save.isEmpty){
+                              return Card(child: Padding(
+                                padding: const EdgeInsets.all(20.0),
                                 child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment: MainAxisAlignment
-                                        .spaceBetween,
-                                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              Text('Buy a new house in New york', style: CredTextStyle.h5,),
-                                              Text('${appModel.formatCurrNoKobo(
-                                                  2500)}/m * 9 month left'),
-                                            ],
-                                          ),
-                                          IconButton(onPressed: () {},
-                                              icon: Icon(Icons
-                                                  .keyboard_arrow_right_outlined))
-
-                                        ],
-                                      ),
-                                      const SizedBox(height: 15,),
-                                      Row(
-                                        children: [
-                                          Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                                              Text('Amount paid', style: CredTextStyle.bs3,),
-                                              Text.rich(TextSpan(
-                                                  text: appModel.formatCurrNoKobo(245),
-                                                  style: CredTextStyle.h2,
-                                                  children: [
-                                                    TextSpan(text: ' out of ${appModel
-                                                        .formatCurrNoKobo(500)}',
-                                                        style: CredTextStyle.bs3.copyWith(fontWeight: FontWeight.w400))
-                                                  ])),
-
-                                            ],
-                                          ),
-                                          const Spacer(),
-                                          Text('35%'),
-                                        ],
-                                      ),
-                                          TweenAnimationBuilder<double>(
-                                            tween: Tween(begin: 0, end: 0.7),
-                                            duration: const Duration(
-                                                seconds: 4),
-                                            builder: (context, value, child) {
-                                              return GradientProgressBar(
-                                                  progress: value);
-                                            },),
-                                    ]
+                                  children: [
+                                    Icon(Icons.not_interested_rounded, size: 80, color: AppColors.error,),
+                                    const SizedBox(height: 15,),
+                                    Text('No Savings '),
+                                    const SizedBox(height:  5,),
+                                    Text('Click the create button to start'),
+                                  ],
                                 ),
-                              ),
-                            )
-                        );
-                      }),
-                    ),
+                              ),);
+                            }
+
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: List.generate(save.length, (index) {
+                                final s = save[index];
+                                return SavingCard(appModel: appModel, saving: s,);
+                              }),
+                            );
+                          })
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 800,)
+            const SizedBox(height: 800),
           ],
         ),
       ),
