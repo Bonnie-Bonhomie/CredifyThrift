b3f1ee528ff29e1adfccc0af833d18886005c0bb
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-09-08 17:04:49 +0100

    Added new color and changed the current color

--- lib/core/constants/app_color.dart
+++ lib/core/constants/app_color.dart
@@ -3,30 +3,39 @@ import 'dart:ui';
 class AppColors {
   AppColors._();
 
-  static const  Color primary = Color(0xFF0730FD);
-  static final Color gradientBtn = Color(0xFF100367);
+  // static const  Color primary = Color(0xFF0730FD);
+  // static final Color gradientBtn = Color(0xFF100367);
+
+  static const  Color primary = Color(0xFF0592CE);
+  static final Color gradientBtn = Color(0xFF04478A);
+
   static const Color darkBlue = Color(0xFF0C0269);
   static const Color lightBlue = Color(0xFF5D4DF6);
   
-  static const Color darkGradient = Color(0xFF1C038C);
-  static const Color lightGradient = Color(0xFF2606FA);
+  // static const Color darkGradient = Color(0xFF1C038C);
+  static const Color darkGradient = Color(0xFF034E8C);
+  static const Color lightGradient = Color(0xFF0694FA);
+  // static const Color lightGradient = Color(0xFF2606FA);
 
-  static const Color progressColor = Color(0xFF0394D3);
+  static const Color progressColor = Color(0xFF0A78D3);
   static const Color grey = Color(0xFF95A9B2);
 
-  static const Color lightBackground = Color(0xFFF6F3EC);
+  static const Color lightBackground = Color(0xFFEDF1F4);
   static const Color darkBackground = Color(0xFF000010);
+
+  static const Color cardLight = Color(0xFFFFFFFF);
+  static const Color cardDark = Color(0xFF161C2C);
   // static const
 
-  static const Color lightButton = Color(0xffE7EBED);
+  static const Color lightButton = Color(0xffE0EEF9);
   static const Color textMain = Color(0xFF02021C);
   static const Color textSecondary = Color(0xFFACACEA);
 
-  static const Color lightGrey = Color(0xFFF8F8FA);
-  static const Color darkGrey = Color(0xFF12182A);
+  static const Color lightGrey = Color(0xFFE7EBEE);
+  static const Color darkGrey = Color(0xFF161C2C);
 
   static const Color blueGrey = Color(0xFF00FFFF);
-  static const Color disableColor = Color(0xF03F5265);
+  static const Color disableColor = Color(0xFFE7EBEE);
   static const Color onSurface = Color(0xFFF3F3FD);
   static const Color surface = Color(0xFF020226);
 
