eb258736e697fa360ca888ec97fec846eb000472
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-09-21 02:51:05 +0100

    Removed the Frequency constant

--- lib/core/constants/app_size.dart
+++ lib/core/constants/app_size.dart
@@ -3,12 +3,3 @@ class AppSize{
   static const double padding = 15;
 }
 
-enum Frequency{
-  daily('day', 'daily', 'Daily'), weekly('week', 'wk', 'Weekly'), monthly('month', 'm', 'Monthly');
-
-  final String abbrev;
-  final String name;
-  final String value;
-
-  const Frequency(this.name, this.abbrev, this.value);
-}
