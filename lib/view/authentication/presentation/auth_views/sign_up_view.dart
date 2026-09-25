01b8dc0ac03c13cd7fc6002acb496b517b8b9da8
Bonnie-Bonhomie <aishatakinyemi234@gmail.com>
2026-09-25 04:32:12 +0100

    Modified

--- lib/view/authentication/presentation/auth_views/sign_up_view.dart
+++ lib/view/authentication/presentation/auth_views/sign_up_view.dart
@@ -86,7 +86,7 @@ class _SignUpViewState extends State<SignUpView> {
                             Text(
                               'Enter your mobile number to create your account or securely sign in.',
                               style: CredTextStyle.bs3.copyWith(
-                                color: AppColors.grey,
+                                color: AppColors.textSecondary,
                                 height: 1.4,
                               ),
                             ).animate().fadeIn(delay: 150.ms).slideY(begin: 0.2, end: 0),
